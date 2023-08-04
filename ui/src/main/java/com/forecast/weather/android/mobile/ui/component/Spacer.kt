ca2e66de3ce3d0dbfcbf4251440e1cb8dcf1a2b5
LottieAnne <kkang_chung125@yahoo.co.jp>
2023-08-04 14:15:51 +0200

    feat: Spacingの作成

---
+++ ui/src/main/java/com/forecast/weather/android/mobile/ui/component/Spacer.kt
@@ -0,0 +1,74 @@
+package com.forecast.weather.android.mobile.ui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+
+fun LazyListScope.simpleVerticalSpacer(
+    height: Dp,
+) {
+    item {
+        Spacer(
+            modifier = Modifier.height(height),
+        )
+    }
+}
+
+@Composable
+fun RowScope.SimpleSpacer(
+    width: Dp,
+) {
+    Spacer(
+        modifier = Modifier.width(width),
+    )
+}
+
+@Composable
+fun ColumnScope.SimpleSpacer(
+    height: Dp,
+) {
+    Spacer(
+        modifier = Modifier.height(height),
+    )
+}
+
+@Composable
+fun RowScope.FillSpacer(
+    modifier: Modifier = Modifier,
+    weight: Float = 1f,
+) {
+    Spacer(
+        modifier = modifier.weight(weight),
+    )
+}
+
+@Composable
+fun ColumnScope.FillSpacer(
+    modifier: Modifier = Modifier,
+    weight: Float = 1f,
+) {
+    Spacer(
+        modifier = modifier.weight(weight),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FillSpacerPreview() {
+    Row {
+        FillSpacer()
+    }
+
+    Column {
+        FillSpacer()
+    }
+}
