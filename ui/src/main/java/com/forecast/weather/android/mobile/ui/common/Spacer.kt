f1c0a9b98945ba5a4201581a29030b9d653a2c78
LottieAnne <kkang_chung125@yahoo.co.jp>
2023-08-06 22:08:58 +0200

    fix: ディレクトリ構成変更

--- ui/src/main/java/com/forecast/weather/android/mobile/ui/common/Spacer.kt
+++ ui/src/main/java/com/forecast/weather/android/mobile/ui/common/Spacer.kt
@@ -1,4 +1,4 @@
-package com.forecast.weather.android.mobile.ui.component
+package com.forecast.weather.android.mobile.ui.common
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
